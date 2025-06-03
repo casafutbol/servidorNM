f9d20d52d0e15abccdf4163844b48887b239bb73
casafutbol <cesjavier@gmail.com>
2025-06-03 14:51:29 +0200

    Vite

---
+++ Front/src/App.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// src/App.tsx
+var react_1 = require("react");
+var App = function () {
+    return (<div>
+      <h1>Hola desde App.tsx</h1>
+    </div>);
+};
+exports.default = App;
